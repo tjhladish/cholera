c2c1f98159da6e1474a870951db08c3a4034c1e8
celestevallejo <Celeste@Celestes-MacBook-Pro.local>
2018-03-16 20:41:30 -0400

    dirty commit -- if header is not defined then compiler can't find constructer

---
+++ Cholera_Sim_LL.h
@@ -0,0 +1,102 @@
+#ifndef CHOLERA_SIR_H
+#define CHOLERA_SIR_H
+
+#include <gsl/gsl_odeiv.h>
+#include <gsl/gsl_errno.h>
+#include <iostream>
+#include <math.h>
+#include "DiffEq_Sim.h"
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+//const vector<double> RAIN = {(267.029+45.824+30.495), 26.68579, 169.00367, 275.74283, 418.06171, 91.20199, 140.94853, 140.94853, 456.00995, 39.24, 98.1, 215.82, 307.38, 18.086, 280.333, 253.204, 352.677, 39.537, 237.222, 263.58, 777.561, 68.464, 136.928, 179.718, 470.69, 11.488, 183.808, 333.152, 620.352, 164.065, 94.985, 198.605, 405.845, 30.348, 106.218, 227.61, 394.524, 10.091, 171.547, 272.457, 544.914, 40.552, 172.346, 263.588, 537.314, 16.198, 121.485, 251.069, 421.148, 110.964, 206.076, 467.634, 515.19, 13.714, 143.997, 198.853, 322.279};
+
+//const vector<double> RAIN ={14.83620868,7.927745098,3.963872549,94.67992717,41.33752801,34.08930392	92.30160364	55.26770868	125.7113866	188.0008123	149.3813683	81.99553501
+
+vector<double> RAIN;
+
+
+//const vector<double> MEAN_RAIN = {8.407,14.214,28.9,46.386,76.279,43.7,60.8,94.15,102.729,207.964,196.021,74.892};//assuming rainfall starts in January
+
+class Cholera_Sim : public DiffEq_Sim {
+
+    private:
+        const double b;
+        const double beta;
+        const double C;
+        const double epsilon;
+        const double gamma;
+        const double mu;
+        const double rho;
+        double meanRain;
+        const double avgWeeksinMonth = 4.33;
+
+    public:
+        Cholera_Sim() : b(0.0), beta(0.0), C(0.0), epsilon(0.0), gamma(0.0), mu(0.0), rho(0.0) { nbins=4; }
+        Cholera_Sim(double _b, double _beta, double _C, double _epsilon, double _gamma, double _mu, double _rho):
+                    b(_b), beta(_beta), C(_C), epsilon(_epsilon), gamma(_gamma), mu(_mu), rho(_rho) { nbins=4; }
+        ~Cholera_Sim() {};
+
+        void initialize(double S, double I, double Y, double R) {
+            x = new double[nbins];
+            x[0] = S; 
+            x[1] = I; 
+            x[2] = Y;
+            x[3] = R;
+        }
+    
+    vector<double> getCompartment(){
+        vector<double> comp;
+        for(unsigned int i = 0; i < nbins - 1; i++){
+            comp.push_back(x[i]);
+        }
+        return comp;
+    }
+    
+    double readRain(){
+        double sumRain = 0.0;
+        if(ifstream in {"Vellore_monthly_rainfall.txt"}){
+            double rain;
+            while(in >> rain){
+                RAIN.push_back(rain);
+                sumRain+=rain;
+            }
+        }
+        RAIN.insert(RAIN.end(), RAIN.begin(), RAIN.end());
+        return meanRain = sumRain/RAIN.size();
+    }
+    
+    double getRain(double t){
+        int rain_idx = (int)t;
+        return RAIN[rain_idx];
+    }
+    
+    double getWeeklyRain(double t){
+        int rain_idx = t/avgWeeksinMonth;
+        return RAIN[rain_idx]/avgWeeksinMonth;
+    }
+    
+
+        void derivative(double const x[], double dxdt[]) {
+            const double S = x[0];
+            const double I = x[1];
+            const double Y = x[2];
+            const double R = x[3];
+            const double N = S+I+Y+R;
+            const double _t = get_time();
+            const double rain = getRain(_t);
+            //const double weekly_rain = getWeeklyRain(_t); //uncomment for weekly
+            const double waterTerm = meanRain/(rain+meanRain);
+            //const double waterTerm = (meanRain/avgWeeksinMonth)/(weekly_rain+perturbation); //uncomment for weekly
+
+            dxdt[0] = (b*N) - waterTerm*(beta*S*(I+Y)/N) - (mu*S) + (rho*Y) + (epsilon*R);
+            dxdt[1] = (waterTerm*C*beta*S*(I+Y)/N) - (gamma*I) - (mu*I);
+            dxdt[2] = (waterTerm*(1-C)*beta*S*(I+Y)/N) - (rho*Y) - (mu*Y);
+            dxdt[3] = (gamma*I) - (epsilon*R) - (mu*R);
+        }
+
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,28 +1,55 @@
-#include "AbcSmc.h"
-#include "Cholera_Sim.h"
+//#include "AbcSmc.h"
+//#include "Cholera_Sim.h"
+//#include "Cholera_Sim_LL.h"
 #include <fstream>
 #include <vector>
+#include <cstring>
 
-const gsl_rng* RNG = gsl_rng_alloc(gsl_rng_taus2);
+#ifdef USE_LL
+#include "Cholera_Sim_LL.h"
+#endif
+
+#ifdef USE_KING
+#include "Cholera_Sim.h"
+#endif
+
+using namespace std;
+
+bool use_LL_model = false;
+bool use_king_model = false;
+
+/*const gsl_rng* RNG = gsl_rng_alloc(gsl_rng_taus2);
 
 vector<double> simulator(vector<double> args, const unsigned long int rng_seed, const unsigned long int serial, const ABC::MPI_par* mp) {
     const double b       = 0.02*1/12;//birth rate
-    const double beta    = (double)(args[0]);//10//transmission rate
-    const double C       = (double)(args[1]);//0.0002;//prob of symptomatic infection
+    //const double beta    = (double)(args[0]);//10//transmission rate
+    //const double C       = (double)(args[1]);//0.0002;//prob of symptomatic infection
+    const double FI = (double)(args[0]);
     
-    const double epsilon = 30.0/583;//waning rate from symptomatic infection
-    const double gamma   = 30.0/14;//recovery rate from symptomatic infection
+    //const double epsilon = 30.0/583;//waning rate from symptomatic infection
+    //const double gamma   = 30.0/14;//recovery rate from symptomatic infection
     const double mu      = 0.02*1/12;//death rate
-    const double rho     = 30.0/102.2;//recovery from asymptomatic infection
+    //const double rho     = 30.0/102.2;//recovery from asymptomatic infection
+    const double gamma_A    = 30.0/1;//asymptomatic recovery rate
+    const double gamma_S    = 30.0/14; //symptomatic recovery rate
+    const double rho_A      = 30.0/102.2; //asympomatic waning rate
+    const double rho_S      = 30.0/583; //symptomatic waning rate
     
     //const double N       = 790590.0/4;
-    const double S       = 33509.20478;
-    const double I       = 0.4518533614;
-    const double Y       = 164119.6172;
-    const double R       = 18.22614054;
+    //const double S       = 33509.20478;
+    //const double I       = 0.4518533614;
+    //const double Y       = 164119.6172;
+    //const double R       = 18.22614054;
+    const double S         = 64912.89976;
+    const double I         = 6.782346297;
+    const double RA        = 689.2424262;
+    const double RS        = 273.5754724;
     
-    Cholera_Sim sim(b, beta, C, epsilon, gamma, mu, rho);
-    sim.initialize(S, I, Y, R);
+    
+    //Cholera_Sim sim(b, beta, C, epsilon, gamma, mu, rho);
+    //sim.initialize(S, I, Y, R);
+    Cholera_Sim_LL sim(b,FI,gamma_A,gamma_S,mu,rho_A,rho_S);
+    sim.initialize(S,I,RA,RS);
     
     const double meanRain = sim.readRain(); //read in rain from txt file and get mean of all rainfall
     //const double weeklyMeanRain = meanRain/4.33; //uncomment for weekly
@@ -43,7 +70,7 @@ vector<double> simulator(vector<double> args, const unsigned long int rng_seed, 
     for (unsigned int i = 0; i < 2*max_time; ++i) {
         if(i >= max_time){
             //sim.printX();
-            metrics[i-168] = gamma*sim.getCompartment()[1];
+            metrics[i-168] = gamma_S*sim.getCompartment()[1];
         }
         sim.step_simulation(1);
         //const double rain = meanRain/(sim.getRain(i)+perturbation);
@@ -96,53 +123,166 @@ int main(int argc, char* argv[]) {
     }
     
     return 0;
-}
+}*/
 
-/*int main() {
+int main(int argc, char** argv) {
     const double b       = 0.02*1/12;//birth rate
-    const double beta    = 10.0;//transmission rate
-    const double C       = 0.0002;//prob of symptomatic infection
-    const double epsilon = 30.0/583;//waning rate from symptomatic infection
-    const double gamma   = 30.0/14;//recovery rate from symptomatic infection
     const double mu      = 0.02*1/12;//death rate
-    const double rho     = 30.0/102.2;//recovery from asymptomatic infection
-
-    //const double N       = 790590.0/4;
-    const double S       = 33509.20478;
-    const double I       = 0.4518533614;
-    const double Y       = 164119.6172;
-    const double R       = 18.22614054;
-
-    Cholera_Sim sim(b, beta, C, epsilon, gamma, mu, rho);
-    sim.initialize(S, I, Y, R);
+    const double N       = 790590.0/4;
     
-    const double meanRain = sim.readRain(); //read in rain from txt file and get mean of all rainfall
-
-    vector <double> monthlyCases;
-    ifstream cases;
-    cases.open("Vellore_monthly_cases.txt");
-    double num;
+    const int max_time   = 168;
+    
+    
+    if(strcmp(argv[0],"./model_LL")==0){
+        const double FI = 239.78;
+        const double gamma_A    = 30.0/.5;//asymptomatic recovery rate
+        const double gamma_S    = 30.0/14; //symptomatic recovery rate
+        const double rho_A      = (1.0/3)*(1.0/12); //asympomatic waning rate
+        const double rho_S      = (1.0/10)*(1.0/12); //symptomatic waning rate
+        
+        //set population distribution
+        const double S         = 64912.89976;
+        const double I         = 6.782346297;
+        const double RA        = 689.2424262;
+        const double RS        = 273.5754724;
+        
+        Cholera_Sim_LL sim(b,FI,gamma_A,gamma_S,mu,rho_A,rho_S);
+        sim.initialize(S,I,RA,RS);
+        
+        const double meanRain = sim.readRain(); //read in rain from txt file and get mean of all rainfall
+        
+        vector <double> monthlyCases;
+        ifstream cases;
+        cases.open("Vellore_monthly_cases.txt");
+        double num;
         while(cases){
             cases >> num;
             monthlyCases.push_back(num);
         }
-    cout<<"length of monthly cases "<<monthlyCases.size()<<"\n";
-    for(int i = 0; i < monthlyCases.size();i++){
-        cout<<"monthly cases["<<i<<"] "<<monthlyCases[i]<<"\n";
-    }
-    
-    const int max_time   = 168;
-
-    for (unsigned int i = 0; i < 2*max_time; ++i) {
-        if(i >= max_time){
-            
-            //cerr<<sim.getCompartment()[1]<<endl;
+        
+        for (unsigned int i = 0; i < 2*max_time; ++i) {
+            if(i >= max_time){
+                
+                cout<<gamma_S*sim.getCompartment()[1]<<endl;
+            }
+            sim.step_simulation(1);
         }
-        sim.step_simulation(1);
-        //const double rain = meanRain/(sim.getRain(i)+perturbation);
-        //cerr << sim.getRain(i) << endl;
+
     }
+    else if(strcmp(argv[0],"./model_king")==0){
+        const double beta    = 608.67;//transmission rate
+        const double C       = 0.000047823;//prob of symptomatic infection
+        const double epsilon = 30.0/583;//waning rate from symptomatic infection
+        const double gamma   = 30.0/14;//recovery rate from symptomatic infection
+        const double rho     = 30.0/102.2;//recovery from asymptomatic infection
+        
+        //set population distribution
+        const double S       = 33509.20478;
+        const double I       = 0.4518533614;
+        const double Y       = 164119.6172;
+        const double R       = 18.22614054;
+        
+        Cholera_Sim sim(b, beta, C, epsilon, gamma, mu, rho);
+        sim.initialize(S, I, Y, R);
+        
+        
+        const double meanRain = sim.readRain(); //read in rain from txt file and get mean of all rainfall
+        
+        vector <double> monthlyCases;
+        ifstream cases;
+        cases.open("Vellore_monthly_cases.txt");
+        double num;
+        while(cases){
+            cases >> num;
+            monthlyCases.push_back(num);
+        }
+        
+        for (unsigned int i = 0; i < 2*max_time; ++i) {
+            if(i >= max_time){
+                
+                cout<<gamma*sim.getCompartment()[1]<<endl;
+            }
+            sim.step_simulation(1);
+        }
+    }
+
+    
+    /*if(use_LL_model){
+        const double FI = 239.78;
+        const double gamma_A    = 30.0/.5;//asymptomatic recovery rate
+        const double gamma_S    = 30.0/14; //symptomatic recovery rate
+        const double rho_A      = (1.0/3)*(1.0/12); //asympomatic waning rate
+        const double rho_S      = (1.0/10)*(1.0/12); //symptomatic waning rate
+        
+        //set population distribution
+        const double S         = 64912.89976;
+        const double I         = 6.782346297;
+        const double RA        = 689.2424262;
+        const double RS        = 273.5754724;
+        
+        Cholera_Sim_LL sim(b,FI,gamma_A,gamma_S,mu,rho_A,rho_S);
+        sim.initialize(S,I,RA,RS);
+        
+        const double meanRain = sim.readRain(); //read in rain from txt file and get mean of all rainfall
+        
+        vector <double> monthlyCases;
+        ifstream cases;
+        cases.open("Vellore_monthly_cases.txt");
+        double num;
+        while(cases){
+            cases >> num;
+            monthlyCases.push_back(num);
+        }
+        
+        for (unsigned int i = 0; i < 2*max_time; ++i) {
+            if(i >= max_time){
+                
+                cout<<gamma_S*sim.getCompartment()[1]<<endl;
+            }
+            sim.step_simulation(1);
+        }
+    }*/
+    /*if(use_king_model){
+        const double beta    = 608.67;//transmission rate
+        const double C       = 0.000047823;//prob of symptomatic infection
+        const double epsilon = 30.0/583;//waning rate from symptomatic infection
+        const double gamma   = 30.0/14;//recovery rate from symptomatic infection
+        const double rho     = 30.0/102.2;//recovery from asymptomatic infection
+        
+        //set population distribution
+        const double S       = 33509.20478;
+        const double I       = 0.4518533614;
+        const double Y       = 164119.6172;
+        const double R       = 18.22614054;
+        
+        Cholera_Sim sim(b, beta, C, epsilon, gamma, mu, rho);
+        sim.initialize(S, I, Y, R);
+        
+        
+        const double meanRain = sim.readRain(); //read in rain from txt file and get mean of all rainfall
+        
+        vector <double> monthlyCases;
+        ifstream cases;
+        cases.open("Vellore_monthly_cases.txt");
+        double num;
+        while(cases){
+            cases >> num;
+            monthlyCases.push_back(num);
+        }
+        
+        for (unsigned int i = 0; i < 2*max_time; ++i) {
+            if(i >= max_time){
+                
+                cout<<gamma*sim.getCompartment()[1]<<endl;
+            }
+            sim.step_simulation(1);
+        }
+    }*/
+    
+   
+
+
 
 
     return 0;
-}*/
+}
